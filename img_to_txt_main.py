74303488f9ca0aaff1d4656decea647a20fcfd26
lipuchen <sphilip1206@gmail.com>
2020-04-16 22:57:21 +0800

    README.md added

--- img_to_txt_main.py
+++ img_to_txt_main.py
@@ -1,7 +1,7 @@
 import pytesseract as tess
 from PIL import Image
 '''
-This file is used to transfer the picture to txt
+This file is used to extract the txt from Image
 '''
 img = Image.open('test.png')
 text = tess.image_to_string(img)
